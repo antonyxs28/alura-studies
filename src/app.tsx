48d1755becf81f2d9bed912832a198ffd370c607
antonyxs28 <antonysantos3286@gmail.com>
2024-12-27 19:06:40 -0300

    feat: create list component

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import { Form } from "./components/form";
+import { List } from "./components/list";
 
 export function App() {
   return (
     <div>
       <h1>Alura studies</h1>
       <Form/>
+      <List/>
     </div>
   )
 }
---
+++ src/components/list/index.tsx
@@ -0,0 +1,21 @@
+export function List() {
+  return (
+    <aside>
+      <h1>Estudos do dia</h1>
+      <ul>
+        <li>
+          <h3>Estudar react</h3>
+          <span>02:00</span>
+        </li>
+        <li>
+          <h3>estudar js</h3>
+          <span>00:30</span>
+        </li>
+        <li>
+          <h3>HTTP state</h3>
+          <span>03:00</span>
+        </li>
+      </ul>
+    </aside>
+  );
+}
